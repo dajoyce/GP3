cb7036f64bf5d83dbc4a9e1ebcd8e13ee473cbe2
Laura Gillespie <lagillespie10@gmail.com>
2019-03-27 21:16:42 -0400

    help i messed up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,109 @@
 import React, { Component } from "react";
+<<<<<<< Updated upstream
+=======
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+
+  input: {
+    display: "none"
+  },
+
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+
+  dense: {
+    marginTop: 16
+  },
+
+  menu: {
+    width: 200
+  }
+});
+
+class Signup extends Component {
+  state = {
+    name: "Cat in the Hat",
+    age: "",
+    multiline: "Controlled",
+    currency: "EUR"
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+>>>>>>> Stashed changes
 
 export default class Signup extends Component {
   render() {
+<<<<<<< Updated upstream
     return <div>Signup</div>;
   }
 }
+=======
+    const { classes } = this.props;
+    console.log(this);
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-name"
+          label="Name"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange("name")}
+          margin="normal"
+          variant="outlined"
+        />
+
+        <TextField
+          id="outlined-email-input"
+          label="Email"
+          className={classes.textField}
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+        />
+
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          className={classes.textField}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
+        />
+
+        <Button variant="contained" color="primary" className={classes.button}>
+          Sign Up
+        </Button>
+      </form>
+    );
+  }
+}
+
+Signup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Signup);
+>>>>>>> Stashed changes

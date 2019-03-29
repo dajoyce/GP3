05d565e6c6b25d462924ab9a1de4bc9daf4a3c80
Ross Gongaware <ross@gongaware.org>
2019-03-28 20:44:38 -0400

    Merged Master

--- client/src/App.js
+++ client/src/App.js
@@ -34,16 +34,12 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
-          <div className="App">
-            {this.state.user && <h1>{this.state.user.email}</h1>}
-            <Login />
-          </div>
+          <div className="App">{this.state.user && <h1>{this.state.user.email}</h1>}</div>
           <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/profile" component={ProfilePage} />
-            <Route exact path="/signup" compoenent={SignupPage} />
-            <Route exact path="/map" component={MapPage} />
-            {/* <Route component={NoMatch} /> */}
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/signup" component={Signup} />
           </Switch>
         </div>
       </Router>
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  }
+});
+
+class Signup extends Component {
+  state = {
+    name: "Jane Smith",
+    email: "",
+    password: ""
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    console.log(this);
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-name"
+          label="Name"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange("name")}
+          margin="normal"
+          variant="outlined"
+        />
+
+        <TextField
+          id="outlined-email-input"
+          label="Email"
+          className={classes.textField}
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+        />
+
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          className={classes.textField}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          href="/signup"
+          className={this.props.classes.button}
+        >
+          Sign Up
+        </Button>
+      </form>
+    );
+  }
+}
+
+Signup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Signup);

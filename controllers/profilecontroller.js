8cc096852f3d962e173292c631331ccab4193d63
JeromeHD <jaymagolan@gmail.com>
2019-03-30 11:20:48 -0400

    mongo

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -3,7 +3,7 @@ const db = require("../models");
 // Defining methods for the UsersController
 module.exports = {
   findById: function(req, res) {
-    db.User.findById(req.params.id)
+    db.User.findOne({ uid: req.params.id })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
@@ -13,7 +13,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.User.findOneAndUpdate({ uid: req.params.id }, { $set: req.body }, { new: true })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
--- routes/api/user.js
+++ routes/api/user.js
@@ -2,12 +2,11 @@ const router = require("express").Router();
 const profilesController = require("../../controllers/profileController");
 
 // Matches with "/api/profile"
-router.route("/").post(profilesController.create);
+router.post("/create", profilesController.create);
 
 // Matches with "/api/profiles/:id"
-router
-  .route("/:id")
-  .get(profilesController.findById)
-  .put(profilesController.update);
+router.get("/:id", profilesController.findById);
+
+router.put("/:id", profilesController.update);
 
 module.exports = router;

4c270dc539717ef0ae77c45c287d2aeb2d223e0f
Ross Gongaware <ross@gongaware.org>
2019-04-11 13:28:44 -0400

    fixed placeTile

--- client/src/components/PlaceTile.js
+++ client/src/components/PlaceTile.js
@@ -3,49 +3,74 @@ import { Typography, Card, CardContent } from '@material-ui/core';
 
 import React, { Component } from 'react'
 
-class PlaceTile extends Component {
-  state = {
-    dimensions: null
-  }
+// class PlaceTile extends Component {
 
-  componentDidMount() {
-    this.setState({
-      dimensions: {
-        width: this.container.offsetWidth,
-      },
-    });
-  }
+//   componentDidMount() {
+//   }
 
-  renderContent() {
-    const { dimensions } = this.state;
+//   renderContent() {
+//     const { dimensions } = this.state;
 
-    return (
-      <div style={{ height: 100, width: dimensions.width }}>
+//     return (
+//       <div style={{ height: 100, width: dimensions.width }}>
+//         <Map
+//           google={this.props.google}
+//           initialCenter={{ lat: this.props.lat, lng: this.props.lng }}
+//           style={{ height: 100, width: dimensions.width }}
+//           disableDefaultUI
+//           draggable={false} />
+//       </div>
+//     );
+//   }
+
+//   render() {
+//     const { dimensions } = this.state;
+//     return (
+//       <div >
+//         <Card >
+//           {dimensions && this.renderContent()}
+//           <CardContent>
+//             <Typography variant="h6">
+//               {this.props.title}
+//             </Typography>
+//           </CardContent>
+//         </Card >
+//       </div>
+//     )
+//   }
+// }
+
+const mapStyle = {
+  position: "absolute",
+  top: 0,
+  height: 100,
+  width: "100%"
+}
+
+const cardStyle = {
+  position: "relative",
+  width: "100%",
+  height: 200
+}
+
+function PlaceTile(props) {
+  return (
+    <div >
+      <Card style={cardStyle}>
         <Map
-          google={this.props.google}
-          initialCenter={{ lat: this.props.lat, lng: this.props.lng }}
-          style={{ height: 100, width: dimensions.width }}
+          google={props.google}
+          initialCenter={{ lat: props.lat, lng: props.lng }}
+          style={mapStyle}
           disableDefaultUI
           draggable={false} />
-      </div>
-    );
-  }
-
-  render() {
-    const { dimensions } = this.state;
-    return (
-      <div ref={el => (this.container = el)}>
-        <Card >
-          {dimensions && this.renderContent()}
-          <CardContent>
-            <Typography variant="h6">
-              {this.props.title}
-            </Typography>
-          </CardContent>
-        </Card >
-      </div>
-    )
-  }
+        <CardContent style={{ position: "absolute", top: 100 }}>
+          <Typography variant="h6">
+            {props.title}
+          </Typography>
+        </CardContent>
+      </Card >
+    </div>
+  )
 }
 
 
